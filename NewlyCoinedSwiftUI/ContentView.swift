7b8b5f85678db3c4d4f5a73cb2befdc86b0e82bf
urijan44 <teamsva360@gmail.com>
2022-05-19 22:07:26 +0900

    Initial Commit

---
+++ NewlyCoinedSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewlyCoinedSwiftUI
+//
+//  Created by hoseung Lee on 2022/05/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewlyCoinedSwiftUI/NewlyCoinedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewlyCoinedSwiftUIApp.swift
+//  NewlyCoinedSwiftUI
+//
+//  Created by hoseung Lee on 2022/05/19.
+//
+
+import SwiftUI
+
+@main
+struct NewlyCoinedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
