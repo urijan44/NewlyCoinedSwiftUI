b8fb7b3b451c616561bceb94c697f924e6fc6faf
urijan44 <teamsva360@gmail.com>
2022-05-22 19:16:32 +0900

    Add SearchFieldView

---
+++ NewlyCoinedView/Sources/NewlyCoinedView/NewlyCoinedSearchField.swift
@@ -0,0 +1,71 @@
+//
+//  NewlyCoinedSearchField.swift
+//
+//
+//  Created by hoseung Lee on 2022/05/22.
+//
+
+import SwiftUI
+
+struct NewlyCoinedSearchField: View {
+
+  @State var imageSize: CGSize = .zero
+  @State var text = ""
+
+  var body: some View {
+    VStack {
+      searchField
+        .padding()
+    }
+  }
+
+  var searchField: some View {
+    GeometryReader { proxy in
+      HStack(spacing: 0) {
+        TextField("신조어를 입력하세요", text: .constant("삼귀자"))
+          .textFieldStyle(NewlyCoinedTextFieldStyle())
+          .anchorPreference(key: ViewSizePreferenceKey.self, value: .bounds) { anchor in
+            ViewSize(id: "TextField", anchor: anchor)
+          }
+          searchButton
+          .frame(height: imageSize.height)
+      }
+      .onPreferenceChange(ViewSizePreferenceKey.self) { value in
+        guard let value = value else { return }
+        imageSize = proxy[value.anchor].size
+      }
+    }
+  }
+
+  var searchButton: some View {
+    SearchButton {
+      text = "search!!"
+    }
+  }
+}
+
+struct NewlyCoinedSearchField_Previews: PreviewProvider {
+  static var previews: some View {
+    NewlyCoinedSearchField()
+  }
+}
+
+struct ViewSize: Equatable, Identifiable {
+  let id: String
+  let anchor: Anchor<CGRect>
+}
+
+struct ViewSizePreferenceKey: PreferenceKey {
+  static var defaultValue: ViewSize?
+  static func reduce(value: inout Value, nextValue: () -> Value) {
+    defaultValue = nextValue()
+  }
+}
+
+struct NewlyCoinedTextFieldStyle: TextFieldStyle {
+  public func _body(configuration: TextField<Self._Label>) -> some View {
+    configuration
+      .padding()
+      .border(.black, width: 2)
+  }
+}
